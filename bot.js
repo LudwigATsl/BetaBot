bfee2eeebfcae4103ce796549a45bd3fd0733d0a
LudwigATsl <46196808+LudwigATsl@users.noreply.github.com>
2018-12-29 18:26:03 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,9 +3,15 @@ const yourID = "527639184941383680";
 const verifyCMD = "!rolle"
 const intCMD = "!interessen"
 
-let initialMessage = `**Akzeptieren der Regeln**`;
+let initialMessage = `**Wähle deine Interessenrollen**`;
 const roles = ["Putzfrau","Logo","Intro"];
 const reactions = ["✅","✅","✅"];
+
+let VinitialMessage = `**Akzeptieren der Regeln**`;
+const Vroles = ["Mitglied"];
+const Vreactions = ["✅"];
+
+
 const Discord = require('discord.js');
 
 //Bot starten... // Status setzen...
@@ -84,55 +90,29 @@ bot.on('raw', event => {
 });
 
 
-
-
-
-
-
-
-
 //Generieren, Absenden und reagieren der Regel Bestätigungs Nachricht
 
+function generateMessages(){
+    var messages = [];
+    messages.push(VinitialMessage);
+    for (let role of Vroles) messages.push(bot.emojis.get("528605335636672512") + `528605335636672512 Abboniere das Interessengebiet **"${role}"** durch einen klick auf die unten aufgeführte Reaktion!`); //DONT CHANGE THIS
+    return messages;
+}
+
+
 bot.on("message", message => {
-    if (message.author.id == yourID && message.content.toLowerCase() == verifyCMD){
-        var toSend = "Reagiere mit EMOJIER ALTAA auf diese Nachricht um Freigeschalten zu werden";
-        message.channel.send(toSend).then( sent => {
-                sent.react("✅");  
-                 
+    if (message.author.id == yourID && message.content.toLowerCase() == intCMD){
+        var toSend = generateMessages();
+        let mappedArray = [[toSend[0], false], ...toSend.slice(1).map( (message, idx) => [message, Vreactions[idx]])];
+        for (let mapObj of mappedArray){
+            message.channel.send(mapObj[0]).then( sent => {
+                if (mapObj[1]){
+                  sent.react(mapObj[1]);  
+                } 
             });
+        }
     }
 })
-
-
-bot.on('raw', event => {
-    if (event.t === 'MESSAGE_REACTION_ADD' || event.t == "MESSAGE_REACTION_REMOVE"){
-        
-        let channel = bot.channels.get(event.d.channel_id);
-        let message = channel.fetchMessage(event.d.message_id).then(msg=> {
-        let user = msg.guild.members.get(event.d.user_id);
-        
-        if (msg.author.id == bot.user.id && msg.content != initialMessage){
-       
-            var re = `\\*\\*"(.+)?(?="\\*\\*)`;
-            var role = msg.content.match(re)[1];
-        
-            if (user.id != bot.user.id){
-                var roleObj = msg.guild.roles.find(r => r.name === role);
-                var memberObj = msg.guild.members.get(user.id);
-                
-                if (event.t === "MESSAGE_REACTION_ADD"){
-                    memberObj.addRole(roleObj)
-                } else {
-                    memberObj.removeRole(roleObj);
-                }
-            }
-        }
-        })
- 
-    }   
-});
-
-
 
 
 
