b49dd7f71e9b59a4396b538846def09c645209e7
LudwigATsl <46196808+LudwigATsl@users.noreply.github.com>
2018-12-29 20:22:21 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -43,7 +43,7 @@ bot.on('ready', () => {
 function generateMessages(){
     var messages = [];
     messages.push(initialMessage);
-    for (let role of roles) messages.push(`<:528605335636672512:> Abboniere das Interessengebiet **"${role}"** durch einen klick auf die unten aufgeführte Reaktion!`); //DONT CHANGE THIS
+    for (let role of roles) messages.push(`<:SL:528604903094878209> Abboniere das Interessengebiet **"${role}"** durch einen klick auf die unten aufgeführte Reaktion!`); //DONT CHANGE THIS
     return messages;
 }
 
